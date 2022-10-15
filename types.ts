12ca5de367f6bffaa4e520abe0c999172d132169
rvaniversario <ruyaniversario@gmail.com>
2022-10-15 10:01:16 +0800

    Inital commit

---
+++ types.ts
@@ -0,0 +1,10 @@
+export interface MyFormValues {
+  username: string;
+  password: string;
+  confirmPassword: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+}
